986b13422d62ff436cec7ad3bc8c2e9787cae2ae
Ignacio de Osma <Ignacio@Ignacios-MacBook-Pro.local>
2019-03-25 07:58:01 -0500

    changes to extra credit

--- student_code.py
+++ student_code.py
@@ -5,159 +5,230 @@ from logical_classes import *
 verbose = 0
 
 class KnowledgeBase(object):
-    def __init__(self, facts=[], rules=[]):
-        self.facts = facts
-        self.rules = rules
-        self.ie = InferenceEngine()
+	def __init__(self, facts=[], rules=[]):
+		self.facts = facts
+		self.rules = rules
+		self.ie = InferenceEngine()
 
-    def __repr__(self):
-        return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
+	def __repr__(self):
+		return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
 
-    def __str__(self):
-        string = "Knowledge Base: \n"
-        string += "\n".join((str(fact) for fact in self.facts)) + "\n"
-        string += "\n".join((str(rule) for rule in self.rules))
-        return string
+	def __str__(self):
+		string = "Knowledge Base: \n"
+		string += "\n".join((str(fact) for fact in self.facts)) + "\n"
+		string += "\n".join((str(rule) for rule in self.rules))
+		return string
 
-    def _get_fact(self, fact):
-        """INTERNAL USE ONLY
-        Get the fact in the KB that is the same as the fact argument
+	def _get_fact(self, fact):
+		"""INTERNAL USE ONLY
+		Get the fact in the KB that is the same as the fact argument
 
-        Args:
-            fact (Fact): Fact we're searching for
+		Args:
+			fact (Fact): Fact we're searching for
 
-        Returns:
-            Fact: matching fact
-        """
-        for kbfact in self.facts:
-            if fact == kbfact:
-                return kbfact
+		Returns:
+			Fact: matching fact
+		"""
+		for kbfact in self.facts:
+			if fact == kbfact:
+				return kbfact
 
-    def _get_rule(self, rule):
-        """INTERNAL USE ONLY
-        Get the rule in the KB that is the same as the rule argument
+	def _get_rule(self, rule):
+		"""INTERNAL USE ONLY
+		Get the rule in the KB that is the same as the rule argument
 
-        Args:
-            rule (Rule): Rule we're searching for
+		Args:
+			rule (Rule): Rule we're searching for
 
-        Returns:
-            Rule: matching rule
-        """
-        for kbrule in self.rules:
-            if rule == kbrule:
-                return kbrule
+		Returns:
+			Rule: matching rule
+		"""
+		for kbrule in self.rules:
+			if rule == kbrule:
+				return kbrule
 
-    def kb_add(self, fact_rule):
-        """Add a fact or rule to the KB
-        Args:
-            fact_rule (Fact|Rule) - the fact or rule to be added
-        Returns:
-            None
-        """
-        printv("Adding {!r}", 1, verbose, [fact_rule])
-        if isinstance(fact_rule, Fact):
-            if fact_rule not in self.facts:
-                self.facts.append(fact_rule)
-                for rule in self.rules:
-                    self.ie.fc_infer(fact_rule, rule, self)
-            else:
-                if fact_rule.supported_by:
-                    ind = self.facts.index(fact_rule)
-                    for f in fact_rule.supported_by:
-                        self.facts[ind].supported_by.append(f)
-                else:
-                    ind = self.facts.index(fact_rule)
-                    self.facts[ind].asserted = True
-        elif isinstance(fact_rule, Rule):
-            if fact_rule not in self.rules:
-                self.rules.append(fact_rule)
-                for fact in self.facts:
-                    self.ie.fc_infer(fact, fact_rule, self)
-            else:
-                if fact_rule.supported_by:
-                    ind = self.rules.index(fact_rule)
-                    for f in fact_rule.supported_by:
-                        self.rules[ind].supported_by.append(f)
-                else:
-                    ind = self.rules.index(fact_rule)
-                    self.rules[ind].asserted = True
+	def kb_add(self, fact_rule):
+		"""Add a fact or rule to the KB
+		Args:
+			fact_rule (Fact|Rule) - the fact or rule to be added
+		Returns:
+			None
+		"""
+		printv("Adding {!r}", 1, verbose, [fact_rule])
+		if isinstance(fact_rule, Fact):
+			if fact_rule not in self.facts:
+				self.facts.append(fact_rule)
+				for rule in self.rules:
+					self.ie.fc_infer(fact_rule, rule, self)
+			else:
+				if fact_rule.supported_by:
+					ind = self.facts.index(fact_rule)
+					for f in fact_rule.supported_by:
+						self.facts[ind].supported_by.append(f)
+				else:
+					ind = self.facts.index(fact_rule)
+					self.facts[ind].asserted = True
+		elif isinstance(fact_rule, Rule):
+			if fact_rule not in self.rules:
+				self.rules.append(fact_rule)
+				for fact in self.facts:
+					self.ie.fc_infer(fact, fact_rule, self)
+			else:
+				if fact_rule.supported_by:
+					ind = self.rules.index(fact_rule)
+					for f in fact_rule.supported_by:
+						self.rules[ind].supported_by.append(f)
+				else:
+					ind = self.rules.index(fact_rule)
+					self.rules[ind].asserted = True
 
-    def kb_assert(self, fact_rule):
-        """Assert a fact or rule into the KB
+	def kb_assert(self, fact_rule):
+		"""Assert a fact or rule into the KB
 
-        Args:
-            fact_rule (Fact or Rule): Fact or Rule we're asserting
-        """
-        printv("Asserting {!r}", 0, verbose, [fact_rule])
-        self.kb_add(fact_rule)
+		Args:
+			fact_rule (Fact or Rule): Fact or Rule we're asserting
+		"""
+		printv("Asserting {!r}", 0, verbose, [fact_rule])
+		self.kb_add(fact_rule)
 
-    def kb_ask(self, fact):
-        """Ask if a fact is in the KB
+	def kb_ask(self, fact):
+		"""Ask if a fact is in the KB
 
-        Args:
-            fact (Fact) - Statement to be asked (will be converted into a Fact)
+		Args:
+			fact (Fact) - Statement to be asked (will be converted into a Fact)
 
-        Returns:
-            listof Bindings|False - list of Bindings if result found, False otherwise
-        """
-        print("Asking {!r}".format(fact))
-        if factq(fact):
-            f = Fact(fact.statement)
-            bindings_lst = ListOfBindings()
-            # ask matched facts
-            for fact in self.facts:
-                binding = match(f.statement, fact.statement)
-                if binding:
-                    bindings_lst.add_bindings(binding, [fact])
+		Returns:
+			listof Bindings|False - list of Bindings if result found, False otherwise
+		"""
+		print("Asking {!r}".format(fact))
+		if factq(fact):
+			f = Fact(fact.statement)
+			bindings_lst = ListOfBindings()
+			# ask matched facts
+			for fact in self.facts:
+				binding = match(f.statement, fact.statement)
+				if binding:
+					bindings_lst.add_bindings(binding, [fact])
 
-            return bindings_lst if bindings_lst.list_of_bindings else []
+			return bindings_lst if bindings_lst.list_of_bindings else []
 
-        else:
-            print("Invalid ask:", fact.statement)
-            return []
+		else:
+			print("Invalid ask:", fact.statement)
+			return []
 
-    def kb_retract(self, fact_or_rule):
-        """Retract a fact from the KB
+	def kb_retract(self, fact_or_rule):
+		"""Retract a fact from the KB
 
-        Args:
-            fact (Fact) - Fact to be retracted
+		Args:
+			fact (Fact) - Fact to be retracted
 
-        Returns:
-            None
-        """
-        printv("Retracting {!r}", 0, verbose, [fact_or_rule])
-        ####################################################
-        # Implementation goes here
-        # Not required for the extra credit assignment
+		Returns:
+			None
+		"""
+		printv("Retracting {!r}", 0, verbose, [fact_or_rule])
+		####################################################
+		# Implementation goes here
+		# Not required for the extra credit assignment
 
-    def kb_explain(self, fact_or_rule):
-        """
-        Explain where the fact or rule comes from
+	def kb_explain(self, fact_or_rule):
+		"""
+		Explain where the fact or rule comes from
 
-        Args:
-            fact_or_rule (Fact or Rule) - Fact or rule to be explained
+		Args:
+			fact_or_rule (Fact or Rule) - Fact or rule to be explained
 
-        Returns:
-            string explaining hierarchical support from other Facts and rules
-        """
-        ####################################################
-        # Student code goes here
+		Returns:
+			string explaining hierarchical support from other Facts and rules
+		"""
+		####################################################
+		# Student code goes here
+		indent = 0
+		result = ""
+		if isinstance(fact_or_rule, Fact):
+			currFact = None
+			currFact = self._get_fact(fact_or_rule)
+			if(currFact == None):
+				result = "Fact is not in the KB"
+				return result
+			else:
+				result += "fact: " + str(currFact.statement)
+				if currFact.asserted:
+					result += " ASSERTED"
+				result += '\n'
+				indent += 1
+				if len(currFact.supported_by) > 0:
+					for supportive in currFact.supported_by:
+						result += self.kb_explain_helper(supportive, indent)
+				return result
 
+		elif isinstance(fact_or_rule, Rule):
+			currRule = None
+			currRule = self._get_rule(fact_or_rule)
+			if (currRule == None):
+				return "Rule is not in the KB"
+			else:
+				result += "rule: ("
+				result += str(currRule.lhs[0])
+				for leftArgument in currRule.lhs[1:]:
+					result += ", " + str(leftArgument)
+				result += ") -> " + str(currRule.rhs)
+				if currRule.asserted:
+					result += " ASSERTED"
+				result += '\n'
+				indent += 1
+				if len(currRule.supported_by) > 0:
+					for support in currRule.supported_by:
+						result += self.kb_explain_helper(support, indent)
+				return result
+		else:
+			return False
+
+	def kb_explain_helper(self, fact_rule, indent):
+		result = ""
+		newIndent = indent
+		for i in range(newIndent):
+			result += "  "
+		result += "SUPPORTED BY" + '\n'
+		newIndent += 1
+		for i in range(newIndent):
+			result += "  "
+		result += "fact: "
+		result += str(fact_rule[0].statement)
+		if fact_rule[0].asserted:
+			result += " ASSERTED"
+		result += '\n'
+		if len(fact_rule[0].supported_by) > 0:
+			for supports in fact_rule[0].supported_by:
+				result += self.kb_explain_helper(supports, newIndent+1)
+		for i in range(newIndent):
+			result += "  "
+		result += "rule: ("
+		result += str(fact_rule[1].lhs[0])
+		for left in fact_rule[1].lhs[1:]:
+			result += ", " +str(left)
+		result += ") -> " + str(fact_rule[1].rhs)
+		if fact_rule[1].asserted:
+			result += " ASSERTED"
+		result += '\n'
+		if len(fact_rule[1].supported_by) > 0:
+			for supports in fact_rule[1].supported_by:
+				result += self.kb_explain_helper(supports, newIndent+1)
+		return result
 
 class InferenceEngine(object):
-    def fc_infer(self, fact, rule, kb):
-        """Forward-chaining to infer new facts and rules
+	def fc_infer(self, fact, rule, kb):
+		"""Forward-chaining to infer new facts and rules
 
-        Args:
-            fact (Fact) - A fact from the KnowledgeBase
-            rule (Rule) - A rule from the KnowledgeBase
-            kb (KnowledgeBase) - A KnowledgeBase
+		Args:
+			fact (Fact) - A fact from the KnowledgeBase
+			rule (Rule) - A rule from the KnowledgeBase
+			kb (KnowledgeBase) - A KnowledgeBase
 
-        Returns:
-            Nothing            
-        """
-        printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
-            [fact.statement, rule.lhs, rule.rhs])
-        ####################################################
-        # Implementation goes here
-        # Not required for the extra credit assignment
+		Returns:
+			Nothing
+		"""
+		printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
+			[fact.statement, rule.lhs, rule.rhs])
+		####################################################
+		# Implementation goes here
+		# Not required for the extra credit assignment
